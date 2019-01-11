23d76c264e184985aa2334bb0ff590b69a94d863
Maximo Bautista <max.bautista97@gmail.com>
2019-01-11 02:01:18 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,46 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |contestants|
+    if contestants.value?("Winner")
+      return contestants["name"].split(" ")[0]
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant.value?(occupation)
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  ok = 0
+  data.each do |season, contestants|
+    ok += contestants.count {|contestant| contestant.value?(hometown)}
+  end
+  ok
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant.value?(hometown)
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages_total = 0 
+  contestants_total = 0
+  data[season].each do |contestants|
+    ages_total += contestants["age"].to_i
+  end
+  (ages_total.to_f / data[season].size.to_f).round
 end
